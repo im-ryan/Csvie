b77c2e35ab2ea354e3dddc562838d847cc83e960
Ryan Huett <ryan@pop-os.localdomain>
2020-06-29 13:36:08 -0700

    Added contract for CsvieCleaner

---
+++ src/Contracts/CsvieCleaner.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Rhuett\Csvie\Contracts;
+
+/**
+ * Interface CsvieCleaner.
+ * 
+ * The interface for making a CsvieCleaner implementation.
+ *
+ * @package Rhuett\Csvie\Contracts;
+ */
+interface CsvieCleaner
+{
+    /**
+     * Cleans the data within a CSV record to match what's expected by the database.
+     * 
+     * @param  \Illuminate\Support\Collection $data
+     * @return \Illuminate\Support\Collection
+     */
+    public function scrub(\Illuminate\Support\Collection $data): \Illuminate\Support\Collection;
+}
